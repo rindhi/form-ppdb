4ffb5748cc999bde7881c8592f998c5d74b96863
Rindhi Dwi Fibrianti <95167082+rindhi@users.noreply.github.com>
2023-11-15 20:24:31 +0700

    commit

---
+++ ppdb.php
@@ -0,0 +1,27 @@
+<article>
+  <div id="form">
+    <form action="index.php?web=insert" method="post">
+      <h1>BUKU TAMU</h1>
+      <label for="nama">Nama :</label>
+      <input type="text" name="nama" id="nama">
+
+      <label for="email">Email :</label>
+      <input type="email" name="email" id="email">
+
+      <label for="ttl">TTL :</label>
+      <input type="text" name="ttl" id="ttl">
+
+      <label for="telp">No.Telp :</label>
+      <input type="text" name="telp" id="telp">
+
+      <label for="alamat">Alamat :</label>
+      <input type="text" name="alamat" id="alamat">
+
+      <label for="pesan">Pesan :</label>
+      <textarea name="pesan" id="pesan" cols="30"></textarea>
+
+      <input type="submit" name="submit" value="Kirim">
+      <input type="reset" name="reset" value="Reset">
+    </form>
+  </div>
+</article>
